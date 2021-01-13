7f109f0a0e03994d910b2c1ccde3ac1129cb85af
fahmi0721 <fahmiidrus131@gmail.com>
2021-01-13 15:01:44 +0800

    Update js.php

--- application/views/modul/request_surat/js.php
+++ application/views/modul/request_surat/js.php
@@ -3,8 +3,44 @@ $(document).ready(function(){
     <?php if(empty($this->uri->segment(2)) || $this->uri->segment(2) == "index"){ ?>
         LoadData();
     <?php } ?>
-	//SearchForm(); 
+    <?php if($this->uri->segment(2) == "approve"){ ?>
+        SearchForm(); 
+    <?php } ?>
+    
+	
 });
+
+
+
+function GetNomorSurat(){
+    var Kode = $("#KodeJenisSurat").val();
+    var TglSurat = $("#TglSurat").val();
+    $.ajax({
+        type : "POST",
+        url : "<?= base_url('request_surat/get_nomor_surat/') ?>",
+        data : "Kode="+Kode+"&TglSurat="+TglSurat,
+        success : function(res){
+            var r = JSON.parse(res);
+            if(r['status'] == "sukses"){
+                $("#NoSurat").val(r['pesan']);
+            }else{
+                Customerror("REQ-SURAT", "001", $res['pesan'],"proses");
+            }
+        },
+        error: function(er){
+            console.log(er);
+        }
+    })
+}
+
+function SearchForm() {
+	$('.select-jenis').select2({
+		allowClear: true,
+		ballowClear: true,
+		theme: "bootstrap",
+		placeholder: 'Pilih Jenis Surat',
+    });
+}
 
 function formatRupiah(angka, prefix){
   var number_string = angka.toString().replace(/[^,\d]/g, ''),
@@ -80,21 +116,43 @@ function LoadData(page){
             if(parseInt(res['jumlah_data']) > 0){
                 var No = parseInt(res['NoAwal']);
                 var html = "";
+                var NoSrt = "";
                 for(var i=0; i < res['data'].length; i++){
                     var iData = res['data'][i];
                     
                     html += "<tr>";
                         html += "<td class='text-center'>"+No+"</td>";
                         html += "<td>";
-                            html += iData['Perihal']+"<br><small>No Dokumen : <b>"+iData['Kode']+"</b></small><br><small>Kepada : <b>"+iData['Kepada']+"</b></small>";
+                            html += iData['Perihal']+"<br><small>No Dokumen : <b>"+iData['NoDokumen']+"</b></small><br><small>Kepada : <b>"+iData['Kepada']+"</b></small>";
                         html += "</td>";
                        
                         html += "<td>";
-                            html += iData['Keterangan'];
+                            html += iData['TglSurat']+"<br><small>Tanggal Approve : <b>"+iData['TglApprove']+"</b></small>";
                         html += "</td>";
                         html += "<td class='text-center'>";
-                            html += "<div class='btn-group'><a href='<?= base_url() ?>jenis_surat/edit/"+iData['Id']+"' class='btn btn-primary btn-xs' data-toggle='tooltip' title='ubah data'><i class='fa fa-edit'></i></a><a href='javascript:void(0)' onclick=\"HapusData('"+iData['Id']+"')\"  class='btn btn-danger btn-xs' data-toggle='tooltip' title='hapus data'><i class='fa fa-trash-o'></i></a></div>";
+                            if(iData['Status'] == "0"){
+                                NoSrt = "nomor surat belum keluar";
+                                html += "<label class='label label-warning'><i class='fa fa-info'></i> Waiting List</label>";
+                            }else{
+                                NoSrt = "<b>"+iData['NoSurat']+"</b>";
+                                html += "<label class='label label-success'><i class='fa fa-check'></i> Terbit</label>";
+                            }
                         html += "</td>";
+                            html += "<td>";
+                                html += NoSrt+"<br><small>Author Request : <b>"+iData['Authorss']+"</b></small><br><small>Direktorat Request : <b>"+iData['DirektoratText']+"</b></small>";
+                            html += "</td>";
+                        html += "<td class='text-center'><div class='btn-group'>";
+                            if(iData['Status'] == "0"){
+                                <?php if($this->session->userdata('KodeLevel') != 2){ ?>
+                                    html += "<a href='<?= base_url() ?>request_surat/approve/"+iData['Id']+"' class='btn btn-success btn-xs' data-toggle='tooltip' title='tambah nomor surat'><i class='fa fa-plus-square'></i></a>";
+                                <?php } ?>
+                                html += "<a href='<?= base_url() ?>request_surat/edit/"+iData['Id']+"' class='btn btn-primary btn-xs' data-toggle='tooltip' title='ubah data'><i class='fa fa-edit'></i></a><a href='javascript:void(0)' onclick=\"HapusData('"+iData['Id']+"')\"  class='btn btn-danger btn-xs' data-toggle='tooltip' title='hapus data'><i class='fa fa-trash-o'></i></a>";
+                                
+                            }else{
+                                html += "-";
+                                
+                            }
+                        html += "</div></td>";
                     html += "</tr>";
                     No++;
                 }
@@ -103,7 +161,7 @@ function LoadData(page){
                 pagination(page, res['jumlah_page']);
                 StopLoad();
             }else{
-                $("#ShowData").html("<tr><td class='text-center' colspan='5'>no data availible in table</td></tr>");
+                $("#ShowData").html("<tr><td class='text-center' colspan='6'>no data availible in table</td></tr>");
                 StopLoad();
             }
         },
@@ -122,7 +180,7 @@ function HapusData(Id){
 }
 
 /** BASIC JS */
-//$("#Tanggal,#tgl_lahir,#tgl_bayar,#tglawal,#tglakhir,#Tanggal1").datepicker({ "format": "yyyy-mm-dd", "autoclose": true });
+$("#TglSurat").datepicker({ dateFormat: "yy-mm-dd", "autoclose": true });
 function StopLoad(){
     $(".LoadingState").hide();
 }
@@ -164,12 +222,18 @@ $("#FormDataUpdate").submit(function(e){
     SubmitDataUpdate();
 })
 
+$("#FormDataApprove").submit(function(e){
+    e.preventDefault();
+    SubmitDataAprove();
+})
+
+
 function Validasi(){
-    var iForm = ["Kode","Jenis"];
-    var iKet = ["Kode belum lengkap!","Jenis belum dipilih"];
+    var iForm = ["Kepada","Perihal","TglSurat"];
+    var iKet = ["Kepada belum lengkap!","Perihal belum lengkap","Tanggal surat belum dipilih"];
     for(i=0; i < iForm.length; i++){
         if($("#"+iForm[i]).val() == ""){
-            Customerror("JNS-SURAT", "001", iKet[i],"proses");
+            Customerror("REQ-SURAT", "001", iKet[i],"proses");
             scrolltop();
             $("#"+iForm[i]).focus();
             return false;
@@ -177,6 +241,60 @@ function Validasi(){
     }
 }
 
+function Validasi(){
+    var iForm = ["Kepada","Perihal","TglSurat"];
+    var iKet = ["Kepada belum lengkap!","Perihal belum lengkap","Tanggal surat belum dipilih"];
+    for(i=0; i < iForm.length; i++){
+        if($("#"+iForm[i]).val() == ""){
+            Customerror("REQ-SURAT", "001", iKet[i],"proses");
+            scrolltop();
+            $("#"+iForm[i]).focus();
+            return false;
+        }
+    }
+}
+
+function ValidasiAprove(){
+    var iForm = ["Kepada","Perihal","TglSurat","KodeJenisSurat","NoSurat"];
+    var iKet = ["Kepada belum lengkap!","Perihal belum lengkap","Tanggal surat belum dipilih","Jenis surat belum dipilih","Nomor surat belum dipilih"];
+    for(i=0; i < iForm.length; i++){
+        if($("#"+iForm[i]).val() == ""){
+            Customerror("REQ-SURAT", "001", iKet[i],"proses");
+            scrolltop();
+            $("#"+iForm[i]).focus();
+            return false;
+        }
+    }
+}
+
+function SubmitDataAprove(){
+    if(ValidasiAprove() != false){
+        var iData = $("#FormDataApprove").serialize();
+        $.ajax({
+            type : "POST",
+            url : "<?= base_url('request_surat/save_approve/') ?>",
+            data : iData,
+            success: function(res){
+                var r = JSON.parse(res);
+                if(r['status'] == "sukses"){
+                    Customsukses("REQ-SURAT", "001", r['pesan'], "proses");
+                    setTimeout(function(){
+                        window.location.href = "<?= base_url('request_surat/index') ?>";
+                    },1000);
+                    scrolltop();
+                    
+                }else{
+                    Customerror("REQ-SURAT", "001", r['pesan'], "proses");
+                    scrolltop();
+                }
+
+            },
+            error: function(er){
+                console.log(er);
+            }
+        })
+    }
+}
 
 function Clear(){
     $(".FormInput").val("");
@@ -187,19 +305,19 @@ function SubmitDataUpdate(){
         var iData = $("#FormDataUpdate").serialize();
         $.ajax({
             type : "POST",
-            url : "<?= base_url('jenis_surat/update/') ?>",
+            url : "<?= base_url('request_surat/update/') ?>",
             data : iData,
             success: function(res){
                 var r = JSON.parse(res);
                 if(r['status'] == "sukses"){
-                    Customsukses("JNS-SURAT", "001", r['pesan'], "proses");
+                    Customsukses("REQ-SURAT", "001", r['pesan'], "proses");
                     setTimeout(function(){
                         $("#proses").html("");
                     },1000);
                     scrolltop();
                     
                 }else{
-                    Customerror("JNS-SURAT", "001", r['pesan'], "proses");
+                    Customerror("REQ-SURAT", "001", r['pesan'], "proses");
                     scrolltop();
                 }
 
@@ -216,16 +334,16 @@ function SubmitData(){
         var iData = $("#FormData").serialize();
         $.ajax({
             type : "POST",
-            url : "<?= base_url('jenis_surat/save/') ?>",
+            url : "<?= base_url('request_surat/save/') ?>",
             data : iData,
             success: function(res){
                 var r = JSON.parse(res);
                 if(r['status'] == "sukses"){
-                    Customsukses("JNS-SURAT", "001", r['pesan'], "proses");
+                    Customsukses("REQ-SURAT", "001", r['pesan'], "proses");
                     scrolltop();
                     Clear();
                 }else{
-                    Customerror("JNS-SURAT", "001", r['pesan'], "proses");
+                    Customerror("REQ-SURAT", "001", r['pesan'], "proses");
                     scrolltop();
                 }
 
@@ -248,18 +366,18 @@ function SubmitDelete(){
     var iData = $("#FormDelete").serialize();
     $.ajax({
         type : "POST",
-        url : "<?= base_url('jenis_surat/delete/') ?>",
+        url : "<?= base_url('request_surat/delete/') ?>",
         data : iData,
         success: function(res){
             var r = JSON.parse(res);
             console.log(r);
             if(r['status'] == "sukses"){
-                Customsukses("JNS-SURAT", "001", r['pesan'], "proses");
+                Customsukses("REQ-SURAT", "001", r['pesan'], "proses");
                 LoadData();
                 scrolltop();
                 ClearModal();
             }else{
-                Customerror("JNS-SURAT", "001", r['pesan'], "proses");
+                Customerror("REQ-SURAT", "001", r['pesan'], "proses");
                 scrolltop();
                 ClearModal();
             }
