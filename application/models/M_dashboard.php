8c2722d350338fd2cfe03f1b755ce996392ba879
fahmi0721 <fahmiidrus131@gamil.com>
2021-01-14 13:14:23 +0800

    update

--- application/models/M_dashboard.php
+++ application/models/M_dashboard.php
@@ -8,12 +8,12 @@ class M_dashboard extends CI_Model {
 
     function load_request_surat_today_by_user($Direktorat){
         $this->db->where("Direktorat",$Direktorat);
-        $this->db->where("DATE_FORMAT(TglCreate,'%Y-%m-%d')",date("Y-m-d"));
+        $this->db->where("DATE_FORMAT(TglCreate,'%Y-%m-%d') =",date("Y-m-d"));
         return $query = $this->db->get('e_office_request_surat',0,10)->result(); 
     }
     
     function jumlah_sm(){
-        $this->db->where("DATE_FORMAT(TglSurat, '%Y')",date("Y"));
+        $this->db->where("DATE_FORMAT(TglSurat, '%Y') =",date("Y"));
         return $query = $this->db->get('e_office_surat_masuk')->num_rows();
     }
 
@@ -25,13 +25,13 @@ class M_dashboard extends CI_Model {
             $this->db->where("KodeDari",$iKode);
             
         }
-        $this->db->where("DATE_FORMAT(TglSurat, '%Y')",$YearNow);
+        $this->db->where("DATE_FORMAT(TglSurat, '%Y') =",$YearNow);
         return $query = $this->db->get('e_office_nota_dinas')->num_rows();
     }
 
     function jumlah_sk(){
         $YearNow = date("Y");
-        $this->db->where("DATE_FORMAT(TglSurat, '%Y')",$YearNow);
+        $this->db->where("DATE_FORMAT(TglSurat, '%Y') = ",$YearNow);
         return $query = $this->db->get('e_office_surat_keluar')->num_rows();
     }
 
@@ -41,7 +41,7 @@ class M_dashboard extends CI_Model {
             $this->db->where("Direktorat",$iKode);
         }
         $YearNow = date("Y");
-        $this->db->where("DATE_FORMAT(TglSurat, '%Y')",$YearNow);
+        $this->db->where("DATE_FORMAT(TglSurat, '%Y') = ",$YearNow);
         return $query = $this->db->get('e_office_request_surat')->num_rows();
     }
     
