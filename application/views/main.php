c8e63e602ba49fd35a89c1e2404c3f124250f6a9
fahmi0721 <fahmiidrus131@gmail.com>
2021-01-17 03:40:21 +0800

    update tampilan dashboard
    Ubah tampilan dashboard

--- application/views/main.php
+++ application/views/main.php
@@ -9,13 +9,77 @@
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class='active'><i class="fa fa-clock-o"></i> <span id='Clock'><?= date("d F Y") ?></span></li>
       </ol>
     </section>
 
     <!-- Main content -->
     <section class="content">
+      <div class="row">
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-aqua"><i class="fa fa-envelope-o"></i></span>
 
+            <div class="info-box-content">
+              <span class="info-box-text">Surat Masuk</span>
+              <span class="info-box-number"><?= generate_number($sm) ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-send-o"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Surat Keluar</span>
+              <span class="info-box-number"><?= generate_number($sk) ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-yellow"><i class="fa fa-envelope-square"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Nota Dinas</span>
+              <span class="info-box-number"><?= generate_number($nd) ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-md-3 col-sm-6 col-xs-12">
+          <div class="info-box">
+            <span class="info-box-icon bg-red"><i class="fa fa-flag"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Request Surat</span>
+              <span class="info-box-number"><?= generate_number($rq) ?></span>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+          <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+      </div>
       
+        <div class='row'>
+            <div class='col-sm-12 col-md-12'>
+                <div class="callout callout-success">
+                    <h4><i class="icon fa fa-info"></i> SELAMAT DATANG!</h4>
+                    <p>HAY <b><?= $this->session->userdata('Nama') ?></b></p>
+                    <p>Apa kabarmu hari ini?, semangat bekerja ya hari ini. </p>
+                </div>
+            </div>
+            
+        </div>
 
     </section>
     <!-- /.content -->
