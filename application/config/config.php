b3f31fa130b5a223ce91c59c4dacc728061137d3
fahmi0721 <fahmiidrus131@gmail.com>
2021-07-11 16:24:24 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kinerja_kampus/';
+$config['base_url'] = 'https://intansejahterautama.co.id/devlop/';
 
 /*
 |--------------------------------------------------------------------------
@@ -158,7 +158,7 @@ $config['composer_autoload'] = FALSE;
 | DO NOT CHANGE THIS UNLESS YOU FULLY UNDERSTAND THE REPERCUSSIONS!!
 |
 */
-$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
+$config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-()@!+=';
 
 /*
 |--------------------------------------------------------------------------
